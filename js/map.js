bce9127a03a7fc94f63f1e85b348e34d8df0df6f
InnaIgorevna <innaigorevnad@gmail.com>
2017-04-09 16:20:55 +0700

    Выполнила вторую задачу 4модуля

--- js/map.js
+++ js/map.js
@@ -239,3 +239,31 @@ pinMap.addEventListener('keydown', function (evt) {
 showPins(offers);
 // Активируем первый пин
 activatePin(document.querySelector('.pin[data-id="0"]'));
+// При изменении времени заезда устанавливаю время выезда
+var selectCheckin = document.querySelector('#time');
+var selectCheckout = document.querySelector('#timeout');
+selectCheckin.addEventListener('change', function () {
+  selectCheckout.value = selectCheckin.value;
+});
+var housingType = document.querySelector('#type');
+var housingPrice = document.querySelector('#price');
+var roomNumber = document.querySelector('#room_number');
+var capacity = document.querySelector('#capacity');
+// При выборе квартиры устанавливаю минимальную цену в зависимости от выбора жилья
+housingType.addEventListener('change', function () {
+  var minPrice = {
+    flat: 1000,
+    house: 10000,
+    bungalo: 0
+  };
+  housingPrice.setAttribute('min', minPrice[housingType.value]);
+});
+// Сравниваю количество комнат с количеством гостей
+roomNumber.addEventListener('change', function () {
+  if (roomNumber.value === '1') {
+    capacity.value = 0;
+  } else {
+    capacity.value = 3;
+  }
+});
+
