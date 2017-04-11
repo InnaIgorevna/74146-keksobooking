bf75de5e6d5ca9fcfe9df787e6a7b4f74a24ebcc
InnaIgorevna <innaigorevnad@gmail.com>
2017-04-11 21:38:47 +0700

    Внесены правки по замечанию наставника

--- js/map.js
+++ js/map.js
@@ -242,13 +242,15 @@ activatePin(document.querySelector('.pin[data-id="0"]'));
 // При изменении времени заезда устанавливаю время выезда
 var selectCheckin = document.querySelector('#time');
 var selectCheckout = document.querySelector('#timeout');
-selectCheckin.addEventListener('change', function () {
-  selectCheckout.value = selectCheckin.value;
-});
 var housingType = document.querySelector('#type');
 var housingPrice = document.querySelector('#price');
 var roomNumber = document.querySelector('#room_number');
 var capacity = document.querySelector('#capacity');
+selectCheckin.addEventListener('change', function () {
+  selectCheckout.value = selectCheckin.value;
+});
+housingType.addEventListener('change', setMinPrice);
+roomNumber.addEventListener('change', setGuestCapacity);
 // При выборе квартиры устанавливаю минимальную цену в зависимости от выбора жилья
 function setMinPrice() {
   var minPrice = {
@@ -258,13 +260,12 @@ function setMinPrice() {
   };
   housingPrice.setAttribute('min', minPrice[housingType.value]);
 }
-housingType.addEventListener('change', setMinPrice);
 // Сравниваю количество комнат с количеством гостей
-roomNumber.addEventListener('change', function () {
+function setGuestCapacity() {
   if (roomNumber.value === '1') {
     capacity.value = 0;
   } else {
     capacity.value = 3;
   }
-});
+}
 
