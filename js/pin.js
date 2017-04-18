83265b487dffdd8e6dfc4e744fdcacf476af7177
InnaIgorevna <innaigorevnad@gmail.com>
2017-04-18 22:40:44 +0700

    Получаем данные с сервера

--- js/pin.js
+++ js/pin.js
@@ -3,10 +3,8 @@ window.pin = (function () {
 // Возращаем <div class"pin"></div> c внутренними данными
   var createPin = function (x, y, src, id) {
     var pin = document.createElement('div');
-    var pinWidth = 56;
-    var pinHeight = 75;
     pin.className = 'pin';
-    pin.style = 'left:' + (x + pinWidth / 2) + 'px; top:' + (y + pinHeight) + 'px';
+    pin.style = 'left:' + x + 'px; top:' + y + 'px';
     pin.innerHTML = '<img src="' + src + '" class="rounded" tabindex="0">';
     pin.setAttribute('data-id', id);
     return pin;
